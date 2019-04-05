9899969c9c56eefa1906766cb34ef1287d2de971
kconnell <connell.kate@ymail.com>
2019-04-04 23:15:51 -0400

    Updates

---
+++ precache-manifest.3033dbd4d9bf8130b1b9fdf723858b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "378e2ac880d6cef7522f",
+    "url": "/my-app/static/js/main.f6076300.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "378e2ac880d6cef7522f",
+    "url": "/my-app/static/css/main.ce34102f.chunk.css"
+  },
+  {
+    "revision": "dc5c72b215ef2c1c7fa8b06bc3169a35",
+    "url": "/my-app/index.html"
+  }
+];
