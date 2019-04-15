affc6bf18f9dced0094a33ea817bf134cb00a828
kconnell <connell.kate@ymail.com>
2019-04-14 21:25:57 -0400

    Worked in app.js

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -14,9 +14,9 @@ function FriendCard(props) {
           </li>
         </ul>
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
+       <span onClick={() => props.removeFriend(props.id)} className="remove">
+         𝘅
+       </span>
     </div>
   );
 }
